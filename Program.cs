313509fb1d3677c00de112c7f1f72c917ec62e36
OVGI <girichevao@gmail.com>
2022-12-28 03:55:16 +0300

    Added task solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк 
+//формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+//Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. 
+
+Console.WriteLine("Введите массив строк через пробел: ");
+string[] firstArray = Console.ReadLine()!.Split(' ');
+string[] secondArray = new string[firstArray.Length];
+
+
+void SecondArrayReceive(string[] firstArray, string[] secondArray)
+{
+    int count = 0;
+    for (int i = 0; i < firstArray.Length; i++)
+    {
+        if (firstArray[i].Length <= 3)
+        {
+            secondArray[count] = firstArray[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+SecondArrayReceive(firstArray, secondArray);
+PrintArray(secondArray);
